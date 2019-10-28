c1fd1898431c587c44411d6337ebb5a3c7f8b712
nikuz <micurino@gmail.com>
2019-10-28 21:30:00 +0100

    added support for development server

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,20 +20,28 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    _getApiUrl();
+    if (kReleaseMode) {
+      _getApiUrl();
+    } else {
+      _loadData(appConfig.apiDebugUrl);
+    }
   }
 
   _getApiUrl() async {
     final response = await http.get(appConfig.apiGetterUrl);
     if (response.statusCode == 200) {
-      appConfig.apiUrl = response.body;
-      setState(() {
-        apiUrlDownloaded = true;
-      });
-      BlocProvider.of<TranslationsBloc>(context).add(TranslationsRequest());
+      _loadData(response.body);
     } else {
       throw Exception('Can\'t get API url');
     }
+  }
+
+  _loadData(String apiUrl) {
+    appConfig.apiUrl = apiUrl;
+    setState(() {
+      apiUrlDownloaded = true;
+    });
+    BlocProvider.of<TranslationsBloc>(context).add(TranslationsRequest());
   }
 
   @override
