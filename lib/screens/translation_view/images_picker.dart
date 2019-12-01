addedb724797b0c583bdd01ef1d5e0c3ff616ee0
nikuz <micurino@gmail.com>
2019-12-01 13:00:01 +0100

    add scroll for active image in image picker

--- lib/screens/translation_view/images_picker.dart
+++ lib/screens/translation_view/images_picker.dart
@@ -19,12 +19,20 @@ class TranslationViewImagePicker extends StatefulWidget {
 class _TranslationViewImagePickerState extends State<TranslationViewImagePicker> {
   TextEditingController _textController;
   TranslationBloc _translationBloc;
+  final itemKey = new GlobalKey();
 
   @override
   void initState() {
     super.initState();
     _textController = TextEditingController(text: widget.word);
     _translationBloc = BlocProvider.of<TranslationBloc>(context);
+    new Future.delayed(Duration(milliseconds: 100), () {
+      Scrollable.ensureVisible(
+        itemKey.currentContext,
+        duration: Duration(seconds: 1),
+        alignment: 0.5
+      );
+    });
   }
 
   @override
@@ -49,65 +57,92 @@ class _TranslationViewImagePickerState extends State<TranslationViewImagePicker>
               );
 
               if (state.imageLoading == false) {
-                imagesList = ListView.builder(
-                  itemBuilder: (BuildContext context, int index) {
-                    final String imageSource = state.images[index];
-
-                    return Center(
-                      child: Container(
-                        margin: EdgeInsets.only(
-                          top: 10,
-                          bottom: 10,
-                        ),
-                        child: FlatButton(
-                          child: Image.memory(getImageBytesFrom64String(imageSource)),
-                          onPressed: () {
-                            _translationBloc.add(
-                              TranslationSelectImage(imageSource)
-                            );
-                            Navigator.pop(context, false);
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                  itemCount: state.images.length,
+                imagesList = SingleChildScrollView(
+                  child: Column(
+                    children: List<Widget>.generate(
+                      state.images.length,
+                      (index) {
+                        final String imageSource = state.images[index];
+                        final bool isActive = state.image == imageSource;
+                        return Container(
+                          key: isActive ? itemKey : Key(index.toString()),
+                          color: isActive ? Colors.greenAccent : Colors.white,
+                          padding: EdgeInsets.only(
+                            top: 10,
+                            bottom: 10,
+                          ),
+                          child: Center(
+                            child: FlatButton(
+                              child: Image.memory(getImageBytesFrom64String(imageSource)),
+                              onPressed: () {
+                                _translationBloc.add(
+                                    TranslationSelectImage(imageSource)
+                                );
+                                Navigator.pop(context, false);
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 );
               }
 
               return Column(
                 children: <Widget>[
                   Container(
-                    color: Colors.white,
-                    child: TextField(
-                      controller: _textController,
-                      autocorrect: false,
-                      textInputAction: TextInputAction.search,
-                      onSubmitted: (String value) {
-                        if (value.length > 1 && value != state.imageSearchWord) {
-                          _translationBloc.add(
-                            TranslationRequestImage(value)
-                          );
-                        }
-                      },
-                      decoration: InputDecoration(
-                        prefixIcon: GestureDetector(
-                          child: Icon(Icons.arrow_back),
-                          onTap: () {
-                            Navigator.pop(context, false);
-                          },
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Color.fromRGBO(0, 0, 0, 0.1),
                         ),
-                        suffixIcon: GestureDetector(
-                          child: Icon(Icons.clear),
-                          onTap: () {
-                            if (_textController.text != '') {
-                              _textController.text = '';
-                            }
-                          },
-                        ),
-                        hintText: 'Search image',
-                        contentPadding: EdgeInsets.only(top: 15),
                       ),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 50,
+                          margin: EdgeInsets.only(right: 10),
+                          child: FlatButton(
+                            child: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.pop(context, false);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: _textController,
+                            autocorrect: false,
+                            textInputAction: TextInputAction.search,
+                            onSubmitted: (String value) {
+                              if (value.length > 1 && value != state.imageSearchWord) {
+                                _translationBloc.add(
+                                    TranslationRequestImage(value)
+                                );
+                              }
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'Search image',
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 50,
+                          margin: EdgeInsets.only(left: 10),
+                          child: FlatButton(
+                            child: Icon(Icons.clear),
+                            onPressed: () {
+                              if (_textController.text != '') {
+                                _textController.text = '';
+                              }
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Expanded(
