8b9703d05e20c83fe28aec5b902380b38e069498
nikuz <micurino@gmail.com>
2020-02-09 16:41:21 +0100

    remove apiUrl update timer to prevent UI refresh

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -82,7 +81,6 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
   bool apiUrlDownloaded = false;
   TabItem _currentTab = TabItem.search;
-  Timer timer;
 
   Map<TabItem, GlobalKey<NavigatorState>> _navigatorKeys = {
     TabItem.search: GlobalKey<NavigatorState>(),
@@ -95,7 +93,6 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
     super.initState();
     if (kReleaseMode) {
       _getApiUrl();
-      timer = new Timer.periodic(Duration(minutes: 1), (Timer t) => _getApiUrl());
     } else {
       _loadData(appConfig.getApiDebugUrl());
     }
@@ -128,7 +125,6 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
 
   @override
   void dispose() {
-    timer.cancel();
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
