a69132481d6562abecf3365ed554043009dbb75f
nikuz <micurino@gmail.com>
2022-12-07 23:13:53 -0800

    rebuild Modals dragging functionality on GestureDetector

--- lib/widgets/modal/modal.dart
+++ lib/widgets/modal/modal.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lingua_flutter/styles/styles.dart';
-import 'package:lingua_flutter/widgets/resize_container/resize_container.dart';
+
+import './modal_content.dart';
 
 class Modal {
   final BuildContext context;
@@ -18,69 +18,22 @@ class Modal {
   }
 
   Future show() {
-    Size? childSize;
-    double screenHeight = MediaQuery.of(context).size.height;
-
     return showGeneralDialog(
       context: context,
       barrierLabel: '',
-      barrierDismissible: true,
+      barrierColor: Colors.transparent,
+      // barrierDismissible: true,
       transitionDuration: const Duration(milliseconds: 100),
       pageBuilder: (BuildContext context, Animation animation, Animation secondaryAnimation) {
-        final MyTheme theme = Styles.theme(context);
-        Widget childWidget = ResizeContainer(
-          onChange: (Size size) {
-            childSize = size;
-          },
+        return ModalContent(
+          isFramed: isFramed,
           child: child,
-        );
-
-        if (isFramed) {
-          final double screenWidth = MediaQuery.of(context).size.width;
-          childWidget = Container(
-            width: screenWidth - screenWidth * 0.2,
-            margin: MediaQuery.of(context).viewInsets,
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: theme.colors.background,
-              borderRadius: BorderRadius.circular(4),
-            ),
-            child: childWidget,
-          );
-        }
-
-        Widget content = Material(
-          type: MaterialType.transparency,
-          child: childWidget,
-        );
-
-        return SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Draggable(
-                maxSimultaneousDrags: 1,
-                axis: Axis.vertical,
-                affinity: Axis.vertical,
-                feedback: content,
-                onDragEnd: (drag) {
-                  if (childSize != null) {
-                    double initialPosition = screenHeight / 2 - childSize!.height / 2;
-                    double offset = (initialPosition - drag.offset.dy).abs();
-
-                    if (offset > 200) {
-                      Modal.dismiss(context);
-                    }
-                  }
-                },
-                childWhenDragging: Container(),
-                child: content,
-              ),
-            ],
-          ),
+          close: () {
+            Modal.dismiss(context);
+          }
         );
       },
     );
   }
 }
+
---
+++ lib/widgets/modal/modal_content.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:lingua_flutter/styles/styles.dart';
+import 'package:lingua_flutter/widgets/resize_container/resize_container.dart';
+
+class ModalContent extends StatefulWidget {
+  final Widget child;
+  final bool isFramed;
+  final VoidCallback close;
+
+  const ModalContent({
+    Key? key,
+    required this.child,
+    this.isFramed = true,
+    required this.close,
+  }) : super(key: key);
+
+  @override
+  State<ModalContent> createState() => _ModalContentState();
+}
+
+class _ModalContentState extends State<ModalContent> {
+  bool _isVisible = false;
+  Offset? _childPosition;
+  double _childDragDelta = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final MyTheme theme = Styles.theme(context);
+    Widget content = widget.child;
+
+    if (widget.isFramed) {
+      final double screenWidth = MediaQuery.of(context).size.width;
+      content = Container(
+        width: screenWidth - screenWidth * 0.2,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: theme.colors.background,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: widget.child,
+      );
+    }
+
+    return Material(
+      type: MaterialType.transparency,
+      child: GestureDetector(
+        onVerticalDragUpdate: (DragUpdateDetails data) {
+          if (_childPosition != null) {
+            setState(() {
+              _childPosition = Offset(_childPosition!.dx, _childPosition!.dy + data.delta.dy);
+              _childDragDelta += data.delta.dy;
+            });
+          }
+        },
+        onVerticalDragEnd: (DragEndDetails data){
+          final mediaContext = MediaQuery.of(context);
+          final visibleScreenSize = mediaContext.size.height - mediaContext.viewInsets.bottom;
+
+          if (_childDragDelta.abs() > visibleScreenSize * 0.1) {
+            widget.close();
+          } else {
+            setState(() {
+              _childPosition = Offset(_childPosition!.dx, _childPosition!.dy - _childDragDelta);
+              _childDragDelta = 0;
+            });
+          }
+        },
+        child: Stack(
+          children: [
+            InkWell(
+              onTap: widget.close,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: const Color(0x80000000),
+              ),
+            ),
+            Positioned(
+              left: _childPosition?.dx,
+              top: (_childPosition?.dy ?? 0) - MediaQuery.of(context).viewInsets.bottom / 2,
+              child: Opacity(
+                opacity: _isVisible ? 1 : 0,
+                child: ResizeContainer(
+                  onChange: (Size childSize) {
+                    Size screenSize = MediaQuery.of(context).size;
+
+                    setState(() {
+                      _childPosition = Offset(
+                        screenSize.width / 2 - childSize.width / 2,
+                        screenSize.height / 2 - childSize.height / 2,
+                      );
+                      _isVisible = true;
+                    });
+                  },
+                  child: content,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
