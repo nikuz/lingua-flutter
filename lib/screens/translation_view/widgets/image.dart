57eb7ae1dde6d60db14864b381434d0cc1db82d4
nikuz <micurino@gmail.com>
2022-12-27 20:30:07 -0800

    revert image resizing for new words to static width and height but without shadow

--- lib/screens/translation_view/widgets/image.dart
+++ lib/screens/translation_view/widgets/image.dart
@@ -4,21 +4,13 @@ import 'package:auto_route/auto_route.dart';
 
 import 'package:lingua_flutter/styles/styles.dart';
 import 'package:lingua_flutter/widgets/image_preview/image_preview.dart';
-import 'package:lingua_flutter/widgets/resize_container/resize_container.dart';
 import 'package:lingua_flutter/screens/router.gr.dart';
 
 import '../bloc/translation_view_cubit.dart';
 import '../bloc/translation_view_state.dart';
 
-class TranslationViewImage extends StatefulWidget {
+class TranslationViewImage extends StatelessWidget {
   const TranslationViewImage({super.key});
-
-  @override
-  State<TranslationViewImage> createState() => _TranslationViewImageState();
-}
-
-class _TranslationViewImageState extends State<TranslationViewImage> {
-  Size _size = const Size(100, 100);
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +22,7 @@ class _TranslationViewImageState extends State<TranslationViewImage> {
           return Container();
         }
 
-        final MyTheme theme = Styles.theme(context);
         String? imageSource = translation.image;
-        final bool isNewWord = translation.id == null;
         final hasImage = imageSource != null || state.imageError != null;
         final editable = translation.id == null || state.imageIsUpdated;
         const constraints = BoxConstraints(
@@ -42,75 +32,39 @@ class _TranslationViewImageState extends State<TranslationViewImage> {
         );
         Widget imageWidget = Container();
 
-        if (hasImage) {
-          imageWidget = Material(
-            color: theme.colors.focusBackground,
-            elevation: editable ? 5 : 0,
-            child: ImagePreview(
-              imageSource: imageSource,
-              errorIconColor: Styles.colors.white,
-              withPreviewOverlay: !editable,
-              onTap: () {
-                if (editable && state.imageSearchWord != null && state.imageError == null) {
-                  AutoRouter.of(context).push(TranslationViewImagePickerRoute(word: state.imageSearchWord!));
-                }
-              },
+        if (state.imageLoading) {
+          imageWidget = const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.white,
             ),
           );
         }
 
-        return Container(
-          constraints: const BoxConstraints(
-            minHeight: 150,
-            minWidth: 150,
-          ),
-          child: Stack(
-            children: [
-              if (state.imageLoading)
-                Center(
-                  child: SizedBox(
-                    width: _size.width,
-                    height: _size.height,
-                    child: const Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
+        if (hasImage) {
+          imageWidget = ImagePreview(
+            imageSource: imageSource,
+            errorIconColor: Styles.colors.white,
+            withPreviewOverlay: !editable,
+            onTap: () {
+              if (editable && state.imageSearchWord != null && state.imageError == null) {
+                AutoRouter.of(context).push(TranslationViewImagePickerRoute(word: state.imageSearchWord!));
+              }
+            },
+          );
+        }
 
-              if (!state.imageLoading && hasImage && isNewWord)
-                Opacity(
-                  opacity: 0,
-                  child: ResizeContainer(
-                    onChange: (Size childSize) {
-                      setState(() {
-                        _size = childSize;
-                      });
-                    },
-                    child: Container(
-                      constraints: constraints,
-                      child: imageWidget,
-                    ),
-                  ),
-                ),
-
-              if (!state.imageLoading && hasImage)
-                Center(
-                  child: AnimatedContainer(
-                    duration: isNewWord ? const Duration(milliseconds: 150) : Duration.zero,
-                    curve: Styles.variables.easeInOut,
-                    width: isNewWord ? _size.width : null,
-                    height: isNewWord ? _size.height : null,
-                    margin: const EdgeInsets.only(
-                      top: 10,
-                      bottom: 10,
-                    ),
-                    constraints: constraints,
-                    child: imageWidget,
-                  ),
-                ),
-            ],
+        return SizedBox(
+          width: 150,
+          height: 150,
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.only(
+                top: 10,
+                bottom: 10,
+              ),
+              constraints: constraints,
+              child: imageWidget,
+            ),
           ),
         );
       },
