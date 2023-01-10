6f944ff49013501a20f9a17bf2ac4cf5adc74d6d
nikuz <micurino@gmail.com>
2023-01-10 08:40:47 -0800

    fix search scroll inaccessibility during scroll position correction animation

--- lib/screens/search/widgets/search_list/search_list.dart
+++ lib/screens/search/widgets/search_list/search_list.dart
@@ -21,7 +21,6 @@ class _SearchListState extends State<SearchList> {
   final _scrollController = ScrollController();
   late SearchCubit _searchCubit;
   late Completer<void> _refreshCompleter;
-  bool _scrollIsCorrected = false;
 
   @override
   void initState() {
@@ -38,9 +37,6 @@ class _SearchListState extends State<SearchList> {
   }
 
   void _onScroll() {
-    if (_scrollIsCorrected) {
-      _scrollIsCorrected = false;
-    }
     if (
       _scrollController.position.pixels > 0.0
       && _scrollController.position.pixels == _scrollController.position.maxScrollExtent
@@ -53,37 +49,36 @@ class _SearchListState extends State<SearchList> {
     }
   }
 
-  bool _onScrollNotification(ScrollNotification notification) {
+  void _correctScrollPosition(_) {
     final searchState = SearchInheritedState.of(context);
     final topPadding = searchState?.getPaddingTop() ?? 0;
+    final scrollPosition = _scrollController.position.pixels;
+    const halfSearchFieldHeight = SearchConstants.searchFieldHeight / 2;
+    final fieldHideScrollPosition = min(_scrollController.position.maxScrollExtent, SearchConstants.searchFieldHeight);
+    double? autoScrollPosition;
+
+    if (scrollPosition > 0 && scrollPosition <= halfSearchFieldHeight) {
+      autoScrollPosition = 0;
+    }
+    else if (scrollPosition > halfSearchFieldHeight && scrollPosition < fieldHideScrollPosition) {
+      autoScrollPosition = topPadding - (topPadding - fieldHideScrollPosition);
+    }
+
+    if (autoScrollPosition != null) {
+      _scrollController.animateTo(
+        autoScrollPosition,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.linearToEaseOut,
+      );
+    }
+  }
+
+  bool _onScrollNotification(ScrollNotification notification) {
+    final searchState = SearchInheritedState.of(context);
     WidgetsBinding.instance.addPostFrameCallback((_) {
       searchState?.broadcastListScroll(notification);
     });
 
-    if (notification is ScrollEndNotification && !_scrollIsCorrected) {
-      _scrollIsCorrected = true;
-      Future.delayed(Duration.zero, () {
-        final scrollPosition = _scrollController.position.pixels;
-        const halfSearchFieldHeight = SearchConstants.searchFieldHeight / 2;
-        final fieldHideScrollPosition = min(_scrollController.position.maxScrollExtent, SearchConstants.searchFieldHeight);
-        double? autoScrollPosition;
-
-        if (scrollPosition > 0 && scrollPosition <= halfSearchFieldHeight) {
-          autoScrollPosition = 0;
-        }
-        else if (scrollPosition > halfSearchFieldHeight && scrollPosition < fieldHideScrollPosition) {
-          autoScrollPosition = topPadding - (topPadding - fieldHideScrollPosition);
-        }
-
-        if (autoScrollPosition != null) {
-          _scrollController.animateTo(
-            autoScrollPosition,
-            duration: const Duration(milliseconds: 200),
-            curve: Curves.linearToEaseOut,
-          );
-        }
-      });
-    }
     return false;
   }
 
@@ -111,48 +106,51 @@ class _SearchListState extends State<SearchList> {
               }
               return _refreshCompleter.future;
             },
-            child: NotificationListener<ScrollNotification>(
-              onNotification: _onScrollNotification,
-              child: ListView.builder(
-                controller: _scrollController,
-                physics: const AlwaysScrollableScrollPhysics(),
-                padding: EdgeInsets.only(top: topPadding),
-                itemCount: state.translations.length + 2,
-                itemBuilder: (BuildContext context, int index) {
-                  if (index == 0) {
-                    return Container(
-                      padding: const EdgeInsets.only(
-                        left: 15,
-                        top: 10,
-                      ),
-                      child: Wrap(
-                        children: [
-                          Text(
-                            state.totalAmount.toString(),
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
+            child: Listener(
+              onPointerUp: _correctScrollPosition,
+              child: NotificationListener<ScrollNotification>(
+                onNotification: _onScrollNotification,
+                child: ListView.builder(
+                  controller: _scrollController,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.only(top: topPadding),
+                  itemCount: state.translations.length + 2,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                      return Container(
+                        padding: const EdgeInsets.only(
+                          left: 15,
+                          top: 10,
+                        ),
+                        child: Wrap(
+                          children: [
+                            Text(
+                              state.totalAmount.toString(),
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            ' word${state.totalAmount > 1 ? 's' : ''}',
-                            style: const TextStyle(fontSize: 15),
-                          ),
-                        ],
-                      ),
+                            Text(
+                              ' word${state.totalAmount > 1 ? 's' : ''}',
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+
+                    if (index - 1 == state.translations.length) {
+                      return const SearchListBottomLoader();
+                    }
+
+                    return SearchListItem(
+                      key: ValueKey('$index-${state.translations[index - 1].updatedAt}'),
+                      translationItem: state.translations[index - 1],
+                      isLast: index == state.translations.length,
                     );
-                  }
-
-                  if (index - 1 == state.translations.length) {
-                    return const SearchListBottomLoader();
-                  }
-
-                  return SearchListItem(
-                    key: ValueKey('$index-${state.translations[index - 1].updatedAt}'),
-                    translationItem: state.translations[index - 1],
-                    isLast: index == state.translations.length,
-                  );
-                },
+                  },
+                ),
               ),
             ),
           );
