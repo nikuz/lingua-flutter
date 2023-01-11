d1fa6127c7e38d9f0c9a186200ad2150f1e3a136
nikuz <micurino@gmail.com>
2023-01-11 00:24:17 -0800

    add local dictionary database index creation on the APP start-up

--- lib/controllers/local_translation.dart
+++ lib/controllers/local_translation.dart
@@ -16,24 +16,36 @@ import 'package:lingua_flutter/utils/string.dart';
 import 'package:lingua_flutter/utils/json.dart';
 
 Future<void> init() async {
-  await DBProvider().rawQuery('''
-    CREATE TABLE IF NOT EXISTS dictionary (
-      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-      word VARCHAR NOT NULL COLLATE NOCASE,
-      pronunciationFrom VARCHAR,
-      pronunciationTo VARCHAR,
-      translation VARCHAR COLLATE NOCASE,
-      raw TEXT NOT NULL,
-      image VARCHAR,
-      translate_from VARCHAR NOT NULL,
-      translate_from_code VARCHAR NOT NULL,
-      translate_to VARCHAR NOT NULL,
-      translate_to_code VARCHAR NOT NULL,
-      schema_version VARCHAR NOT NULL,
-      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-      updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+  await DBProvider().batchQuery([
+    const BatchQueryRequest(
+      type: 'rawQuery',
+      query: '''
+        CREATE TABLE IF NOT EXISTS dictionary (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        word VARCHAR NOT NULL COLLATE NOCASE,
+        pronunciationFrom VARCHAR,
+        pronunciationTo VARCHAR,
+        translation VARCHAR COLLATE NOCASE,
+        raw TEXT NOT NULL,
+        image VARCHAR,
+        translate_from VARCHAR NOT NULL,
+        translate_from_code VARCHAR NOT NULL,
+        translate_to VARCHAR NOT NULL,
+        translate_to_code VARCHAR NOT NULL,
+        schema_version VARCHAR NOT NULL,
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+      )
+      ''',
+    ),
+    const BatchQueryRequest(
+      type: 'rawQuery',
+      query: '''
+        CREATE INDEX IF NOT EXISTS dictionary_index
+        ON dictionary(word, translation)
+      ''',
     )
-  ''');
+  ]);
 }
 
 Future<int> getListLength() async {
